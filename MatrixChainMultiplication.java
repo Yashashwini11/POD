6493ea1a5b19273d4a227c5e386ea5f1526791a8
Yashashwini11 <125336223+Yashashwini11@users.noreply.github.com>
2024-09-25 06:39:33 +0530

    Update MatrixChainMultiplication.java

--- MatrixChainMultiplication.java
+++ MatrixChainMultiplication.java
@@ -1,3 +1,9 @@
+///question
+Given the dimension of a sequence of matrices in an array arr[], where the dimension of the ith matrix is (arr[i-1] * arr[i]), 
+the task is to find the most efficient way to multiply these matrices together such that the total number of element multiplications is minimum. 
+When two matrices of size m*n and n*p when multiplied, they generate a matrix of size m*p and the number of multiplications performed are m*n*p
+
+
 // You are using Java
 import java.io.*;
 import java.util.*;
